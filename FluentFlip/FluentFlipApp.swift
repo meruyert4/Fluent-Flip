3f78060473690edf5dd923a712909134e8a531fe
meruyert4 <meruyertbauyrzhanqyzy@gmail.com>
2025-02-08 14:07:08 +0500

    Initial Commit

---
+++ FluentFlip/FluentFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluentFlipApp.swift
+//  FluentFlip
+//
+//  Created by Meruyert Boranbay on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FluentFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FluentFlipTests/FluentFlipTests.swift
@@ -0,0 +1,17 @@
+//
+//  FluentFlipTests.swift
+//  FluentFlipTests
+//
+//  Created by Meruyert Boranbay on 08.02.2025.
+//
+
+import Testing
+@testable import FluentFlip
+
+struct FluentFlipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
